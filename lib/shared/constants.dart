99488a862f6768ec24ea150a7169a92440fbcfaa
Quy Trân <quyt40247@gmail.com>
2024-09-19 07:25:13 +0700

    chat funtion

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:865118613327:web:c04bf092d5360743019f3a";
+  static String apiKey = "AIzaSyCcr_ea_WmXQY4f6MSvr7qdaEA7nBcZtGk";
+  static String messagingSenderId = "865118613327";
+  static String projectId = "chatappflutter-e33ba";
+  final primaryColor = const Color(0xFFee7b64);
+}
